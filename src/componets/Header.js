e54a4a39c32dcbad55bfbf0a0b5382de08877fa9
Nikolaos Tsoukas <nick.tsoukas101@gmail.com>
2020-12-28 21:21:29 -0600

    Adding background image

--- src/componets/Header.js
+++ src/componets/Header.js
@@ -2,8 +2,7 @@ import { Component, useContext, useState } from 'react';
 import '../index.css';
 import '../App.css'
 import ImageContext from '../context/imageContext';
-const testImage = 'https://images.unsplash.com/photo-1561655544-84c345cf6308?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=2690&q=80'
-
+import back  from '../freckles.jpeg'
 const Header = (props) => {
     const [image, setPic ] = useState('');
     const { setImage } = useContext(ImageContext);
@@ -13,7 +12,7 @@ const Header = (props) => {
         setPic('')
     }
     return(
-        <div className="flex h-96 header justify-center items-center">
+        <div style={{backgroundImage: `url(${back})`}} className="flex h-96 header justify-center items-center">
     
         <form onSubmit={handleSubmit}>
             <input 
