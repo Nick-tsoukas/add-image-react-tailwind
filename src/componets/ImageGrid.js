6ca8c5b09b18e212f0f63d6a2b4856495645f97f
Nikolaos Tsoukas <nick.tsoukas101@gmail.com>
2020-12-28 21:00:53 -0600

    ...

--- src/componets/ImageGrid.js
+++ src/componets/ImageGrid.js
@@ -28,7 +28,6 @@ const ImageGrid = (props) => {
     },[]);
 
     return(
-<<<<<<< HEAD
         <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 justify-items-center m-6 gap-4 bg-gray-900">
             {
           images.map((image, index) => {
@@ -38,15 +37,6 @@ const ImageGrid = (props) => {
                          <img src={image.img} key={index} className="" />
                        </div>
                     </div>
-=======
-        // <div className="grid grid-cols-3 justify-items-center m-6 gap-10">
-        <div className="image_container">
-            
-            {
-                images.map((image, index) => {
-                    return(
-                        <img className="image" src={image.img} key={index} />
->>>>>>> mason
                     )
                 })
             }
