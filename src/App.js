29bf12f8cbb376c5bb9a30c5fb44cd21400b0dd7
Nikolaos Tsoukas <nick.tsoukas101@gmail.com>
2020-12-28 20:52:29 -0600

    ...

--- src/App.js
+++ src/App.js
@@ -9,31 +9,6 @@ import ImageContext from './context/imageContext';
 
 
 function App() {
-  // const [girlPics, setGirlPics ] = useState([]);
-  // This will only run once at page load the useEffect hook
-// useEffect(() => {
-    // const getRedheads = async () => {
-    //   return await fetch("https://esits.org/tumblr-json.php?url=https://beautiful-redheads.tumblr.com", {method: 'GET'})
-    //     .then(res => res.json())
-    //     .then(res => res.posts
-    //       .filter(post => post.type ==='photo')
-    //       .map(photo => {
-    //         return {
-    //           img: photo["photo-url-1280"],
-    //           imgThumb: photo["photo-url-1280"]
-    //         }
-    //       })
-    //     )
-    //     .catch(error => console.log('WTF!', error))
-    // }
-    
-    // getRedheads()
-    //   // .then(redheads => {console.log(redheads)});
-    //   // .then((redheads) => setGirlPics((prePics) => [...redheads]));
-
-    //   // Add the empty array as a second arg to only run once or else it will always run on any rerender 
-//   },[]);
-
   return (
     <ImageProvider>
       <div className="h-auto w-screen">
